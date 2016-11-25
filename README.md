4ec4554dec19e308eed7d50ff085a62d01c2ea1f
mkoryakov <mnkoryakov@yandex.ru>
2016-11-26 00:58:13 +0300

    На проверку.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # 12_image_resize
+Скрипт принимает на вход путь к изображению (обязательный параметр) и изменяет его размеры в соответствии с заданными пользователем значениями. Необязательные параметры: width - ширина результирующей картинки, height - её высота, scale - во сколько раз увеличить изображение (может быть меньше 1), output - куда класть результирующий файл.
+
+Примеры использования:
+python image_resize.py picture.png --height 150 --output new_picture.png
+python image_resize.py picture.png --height 150 --width 200
+python image_resize.py picture.png --scale 0.8 
--- image_resize.py
+++ image_resize.py
@@ -1,8 +1,65 @@
+import argparse
+import os.path
+from PIL import Image
 
 
-def resize_image(path_to_original, path_to_result):
-    pass
+def get_new_filepath(filepath, width, height):
+    root, extension = os.path.splitext(filepath)
+    root += '__%dx%d' % (width, height)
+    return root + extension
+
+
+def resize_image(path_to_original, path_to_result,
+                 new_scale, new_width, new_height):
+    if new_scale and (new_width or new_height):
+        return '''Не могут быть одновременно указаны аргументы
+        scale и width/height'''
+    original_picture = Image.open(path_to_original)
+    old_width = original_picture.size[0]
+    old_height = original_picture.size[1]
+    warning = ''
+    if new_scale:
+        new_width = old_width * new_scale
+        new_height = old_height * new_scale
+    elif new_width and new_height:
+        if new_width / new_height != old_width / old_height:
+            warning = '''Введённые пропорции width/height не совпадают
+            с пропорциями оригинального изображения\n'''
+    elif new_width:
+        new_height = new_width * old_height / old_width
+    else:
+        new_width = new_height * old_width / old_height
+    new_size = int(new_width), int(new_height)
+    result_picture = original_picture.resize(new_size)
+    if not path_to_result:
+        path_to_result = get_new_filepath(path_to_original,
+                                          new_width, new_height)
+    result_picture.save(path_to_result)
+    status_message = '''Изображение с новыми размерами сохранено
+    в файл с именем %s''' % path_to_result
+    return warning + status_message
 
 
 if __name__ == '__main__':
-    pass
+    parser = argparse.ArgumentParser(prog='image_resize',
+                                     description='''Скрипт, изменяющий размер
+                                     картинки в соответствии с заданным
+                                     масштабом''')
+    parser.add_argument('image', help='путь к исходному изображению')
+    parser.add_argument('--width', type=int, default=0,
+                        help='новая ширина изображения')
+    parser.add_argument('--height', type=int, default=0,
+                        help='новая высота изображения')
+    parser.add_argument('--scale', type=float, default=0.0,
+                        help='коэффициент сжатия/растяжения изображения')
+    parser.add_argument('--output', default='',
+                        help='имя изменённого изображения')
+    arguments = parser.parse_args()
+    path_to_original = arguments.image
+    new_scale = arguments.scale
+    new_width = arguments.width
+    new_height = arguments.height
+    path_to_result = arguments.output
+    status_message = resize_image(path_to_original, path_to_result,
+                                  new_scale, new_width, new_height)
+    print(status_message)
