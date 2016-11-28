a9839945d2f17975c74952d03b3bed8b336c77f2
mkoryakov <mnkoryakov@yandex.ru>
2016-11-29 01:40:40 +0300

    Модифицировал код так, чтобы каждая функция занималась чем-то одним.

--- image_resize.py
+++ image_resize.py
@@ -3,44 +3,48 @@ import os.path
 from PIL import Image
 
 
-def get_new_filepath(filepath, width, height):
+def is_valid_program_arguments(scale, width, height):
+    if scale and (width or height):
+        return '''Не могут быть одновременно указаны аргументы
+        scale и width/height'''
+
+
+def get_new_filepath(filepath, size):
     root, extension = os.path.splitext(filepath)
-    root += '__%dx%d' % (width, height)
-    return root + extension
+    root = '%s__%dx%d' % (root, size[0], size[1])
+    return '%s%s' % (root, extension)
+
+
+def get_new_size(old_size, new_scale, new_width, new_height):
+    if new_scale:
+        new_width = old_size[0] * new_scale
+        new_height = old_size[1] * new_scale
+    elif new_width and new_height:
+        if new_width / new_height != old_size[0] / old_size[1]:
+            print('''Введённые пропорции width/height не совпадают
+            с пропорциями оригинального изображения''')
+    elif new_width:
+        new_height = new_width * old_size[1] / old_size[0]
+    else:
+        new_width = new_height * old_size[0] / old_size[1]
+    return int(new_width), int(new_height)
 
 
 def resize_image(path_to_original, path_to_result,
                  new_scale, new_width, new_height):
-    if new_scale and (new_width or new_height):
-        return '''Не могут быть одновременно указаны аргументы
-        scale и width/height'''
     original_picture = Image.open(path_to_original)
-    old_width = original_picture.size[0]
-    old_height = original_picture.size[1]
-    warning = ''
-    if new_scale:
-        new_width = old_width * new_scale
-        new_height = old_height * new_scale
-    elif new_width and new_height:
-        if new_width / new_height != old_width / old_height:
-            warning = '''Введённые пропорции width/height не совпадают
-            с пропорциями оригинального изображения\n'''
-    elif new_width:
-        new_height = new_width * old_height / old_width
-    else:
-        new_width = new_height * old_width / old_height
-    new_size = int(new_width), int(new_height)
+    old_size = original_picture.size
+    new_size = get_new_size(old_size, new_scale, new_width, new_height)
     result_picture = original_picture.resize(new_size)
     if not path_to_result:
-        path_to_result = get_new_filepath(path_to_original,
-                                          new_width, new_height)
+        path_to_result = get_new_filepath(path_to_original, new_size)
     result_picture.save(path_to_result)
     status_message = '''Изображение с новыми размерами сохранено
     в файл с именем %s''' % path_to_result
-    return warning + status_message
+    return status_message
 
 
-if __name__ == '__main__':
+def get_program_arguments():
     parser = argparse.ArgumentParser(prog='image_resize',
                                      description='''Скрипт, изменяющий размер
                                      картинки в соответствии с заданным
@@ -55,11 +59,17 @@ if __name__ == '__main__':
     parser.add_argument('--output', default='',
                         help='имя изменённого изображения')
     arguments = parser.parse_args()
-    path_to_original = arguments.image
-    new_scale = arguments.scale
-    new_width = arguments.width
-    new_height = arguments.height
-    path_to_result = arguments.output
-    status_message = resize_image(path_to_original, path_to_result,
-                                  new_scale, new_width, new_height)
-    print(status_message)
+    return (arguments.image, arguments.scale, arguments.width,
+            arguments.height, arguments.output)
+
+
+if __name__ == '__main__':
+    (path_to_original, new_scale, new_width, new_height,
+     path_to_result) = get_program_arguments()
+    error = is_valid_program_arguments(new_scale, new_width, new_height)
+    if error:
+        print(error)
+    else:
+        status_message = resize_image(path_to_original, path_to_result,
+                                      new_scale, new_width, new_height)
+        print(status_message)
